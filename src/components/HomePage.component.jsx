984c646c9cdcca4bea61f1b51ad7ff1a4c6f1fca
kap1147 <joshua@theyardapp.com>
2021-10-05 08:04:40 -0400

    removed auth check

--- src/components/HomePage.component.jsx
+++ src/components/HomePage.component.jsx
@@ -10,7 +10,7 @@ import {
   Grid,
   CircularProgress,
   makeStyles,
-  Typography
+  Typography,
 } from "@material-ui/core";
 
 let data = [
@@ -19,36 +19,33 @@ let data = [
     avatar:
       "https://static.wikia.nocookie.net/fortnite/images/f/fe/Fishstick_%28New%29_-_Outfit_-_Fortnite.png",
     name: "John Smith",
-    username: "fishstick"
+    username: "fishstick",
   },
   {
-    img:
-      "https://www.wallpapertip.com/wmimgs/0-3002_very-cool-wallpapers-cool-backgrounds-of-mermaids.jpg",
+    img: "https://www.wallpapertip.com/wmimgs/0-3002_very-cool-wallpapers-cool-backgrounds-of-mermaids.jpg",
     avatar: "https://wallpapercave.com/wp/wp7171957.jpg",
     name: "Aleena Rosado",
-    username: "AnimeChick"
+    username: "AnimeChick",
   },
   {
-    img:
-      "https://images.squarespace-cdn.com/content/v1/5c5c3833840b161566b02a76/1573133725500-Y5PCN0V04I86HDAT8AT0/WBC_7095.jpg",
+    img: "https://images.squarespace-cdn.com/content/v1/5c5c3833840b161566b02a76/1573133725500-Y5PCN0V04I86HDAT8AT0/WBC_7095.jpg",
     avatar:
       "http://blog.lemnsissay.com/wp-content/uploads/2013/11/167675-south-park-chef-2.gif",
     name: "Jerome Mcelroy",
-    username: "chef"
+    username: "chef",
   },
   {
-    img:
-      "https://evofitness.ch/wp-content/uploads/2019/01/Crossfit-EVO-Fitness-1200x675.jpg",
+    img: "https://evofitness.ch/wp-content/uploads/2019/01/Crossfit-EVO-Fitness-1200x675.jpg",
     avatar:
       "https://64.media.tumblr.com/5702e0d42227e289b8ce9f1377a7d7d7/tumblr_pbiibzZUyT1rq9ihbo1_1280.jpg",
     name: "Heather Robers",
-    username: "MissFit"
-  }
+    username: "MissFit",
+  },
 ];
 
 let useStyles = makeStyles((theme) => ({
   root: {
-    height: "100%"
+    height: "100%",
   },
   body: {
     flex: 1,
@@ -57,47 +54,40 @@ let useStyles = makeStyles((theme) => ({
     MsOverflowStyle: "none",
     scrollbarWidth: "none",
     "&::-webkit-scrollbar": {
-      display: "none"
-    }
+      display: "none",
+    },
   },
   row: {
     marginBottom: "25px",
-    background: "#fff"
+    background: "#fff",
   },
   subtitle: {
     padding: "10px 0",
-    fontWeight: 600
+    fontWeight: 600,
   },
   text: {
-    color: theme.palette.grey[600]
+    color: theme.palette.grey[600],
   },
   post_grid: {
     display: "grid",
     gridTemplateColumns: "50% 50%",
     gridTemplateRows: "auto",
     gridAutoRows: "auto",
-    gap: "5px"
+    gap: "5px",
   },
   postImage: {
-    content: "cover"
+    content: "cover",
   },
   postImg: {
     width: "100%",
     height: "100%",
-    objectFit: "contain"
-  }
+    objectFit: "contain",
+  },
 }));
 
-const HomePage = ({ history }) => {
-  const { isAuthenticated } = useSelector((state) => state.user);
+const HomePage = () => {
   const { posts } = useSelector((state) => state.database);
   let classes = useStyles();
-
-  useEffect(() => {
-    if (!isAuthenticated) {
-      history.push("/landing");
-    }
-  }, [history, isAuthenticated]);
 
   return (
     <Grid container direction="column" className={classes.root}>
@@ -148,7 +138,7 @@ const HomePage = ({ history }) => {
           )}
         </Grid>
       </Grid>
-      <Footer history={history} />
+      <Footer />
     </Grid>
   );
 };
