5e39f5807d2a544940c2d5bae21e6b0b4874c206
kap1147 <joshua@theyardapp.com>
2021-10-05 09:03:43 -0400

    removed hard data going to PostSlider

--- src/components/HomePage.component.jsx
+++ src/components/HomePage.component.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 // Npm
 import { useSelector } from "react-redux";
 // Components
@@ -12,36 +12,6 @@ import {
   makeStyles,
   Typography,
 } from "@material-ui/core";
-
-let data = [
-  {
-    img: "https://www.enjpg.com/img/2020/cool-for-boys-4.jpg",
-    avatar:
-      "https://static.wikia.nocookie.net/fortnite/images/f/fe/Fishstick_%28New%29_-_Outfit_-_Fortnite.png",
-    name: "John Smith",
-    username: "fishstick",
-  },
-  {
-    img: "https://www.wallpapertip.com/wmimgs/0-3002_very-cool-wallpapers-cool-backgrounds-of-mermaids.jpg",
-    avatar: "https://wallpapercave.com/wp/wp7171957.jpg",
-    name: "Aleena Rosado",
-    username: "AnimeChick",
-  },
-  {
-    img: "https://images.squarespace-cdn.com/content/v1/5c5c3833840b161566b02a76/1573133725500-Y5PCN0V04I86HDAT8AT0/WBC_7095.jpg",
-    avatar:
-      "http://blog.lemnsissay.com/wp-content/uploads/2013/11/167675-south-park-chef-2.gif",
-    name: "Jerome Mcelroy",
-    username: "chef",
-  },
-  {
-    img: "https://evofitness.ch/wp-content/uploads/2019/01/Crossfit-EVO-Fitness-1200x675.jpg",
-    avatar:
-      "https://64.media.tumblr.com/5702e0d42227e289b8ce9f1377a7d7d7/tumblr_pbiibzZUyT1rq9ihbo1_1280.jpg",
-    name: "Heather Robers",
-    username: "MissFit",
-  },
-];
 
 let useStyles = makeStyles((theme) => ({
   root: {
@@ -86,8 +56,22 @@ let useStyles = makeStyles((theme) => ({
 }));
 
 const HomePage = () => {
-  const { posts } = useSelector((state) => state.database);
+  const [data, setData] = useState([]);
+  const { posts, users } = useSelector((state) => state.database);
   let classes = useStyles();
+
+  useEffect(() => {
+    if (!data.length && users.length && posts.length) {
+      let addData = [];
+      users.forEach((user, i) => {
+        if (i % 2) {
+          let img = posts.find((post) => post.userId === user.id);
+          addData.push({ ...user, img: img.image, postId: img.id });
+        }
+      });
+      setData(addData);
+    }
+  }, [data, users]);
 
   return (
     <Grid container direction="column" className={classes.root}>
